e61db929fc1ea838d43a42a2766f583f7a2d4574
Sanjeev <sanjeevstunner1@gmail.com>
2022-10-07 01:24:08 +0000

    [TMP] Add config with flags enabled

--- config.mk
+++ config.mk
@@ -1,5 +1,7 @@
 # Inherit from apex config
+TARGET_SHIP_PREBUILT_APEX := true
 $(call inherit-product, vendor/pixel-additional/configs/apex.mk)
 
 # Inherit from telephony config
+TARGET_INCLUDE_CARRIER_SETTINGS := true
 $(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
